d17435cd96bc61f3cd4c68cf7ac1c276823dda3c
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-25 01:47:41 +0200

    dev ops v0.0A.00

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# v0.00 create file
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
