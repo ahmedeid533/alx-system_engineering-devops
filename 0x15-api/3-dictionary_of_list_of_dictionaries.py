30ed9a9308b616a0cd724357f95a2abf11d6164e
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-02 23:40:34 +0200

    dev ops rest api v0.0B

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,16 +12,14 @@ if __name__ == "__main__":
     users = requests.get(url).json()
 
     for user in users:
-        url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
             user['id'])
         tasks = requests.get(url).json()
-
-        completed_tasks = [task for task in tasks if task.get('completed')
-                           is True]
-
-        AllempTasks.append({user['id']: [{
-            'username': user.get('username'),
-            'task': task.get('title'),
-            'completed': task.get('completed')} for task in tasks]})
+        tasks_list = []
+        for task in tasks:
+            tasks_list.append({"username": user['username'],
+                               "task": task['title'],
+                               "completed": task['completed']})
+        AllempTasks.append({user['id']: tasks_list})
     with open('todo_all_employees.json', 'w') as jsonfile:
         json.dump(AllempTasks, jsonfile)
