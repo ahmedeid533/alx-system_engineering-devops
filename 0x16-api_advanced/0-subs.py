b26e59c6a042c093ccfa1a3a5df6e174a1cdbec0
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-16 14:14:15 +0200

    adv api v0.006

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     import requests
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': "My-User-Agent"}
+    headers = {'User-Agent': 'My-User-Agent'}
     r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code >= 300:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'My-User-Agent'}
     r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code >= 300:
-        print("None")
-        return
-    for post in r.json().get('data').get('children'):
-        print(post.get('data').get('title'))
+        print('None')
+    else:
+        for post in r.json().get('data').get('children'):
+            print(post.get('data').get('title'))
