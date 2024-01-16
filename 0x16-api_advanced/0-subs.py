979d2c907175277696d8fee7d95784786ba95575
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-16 14:44:38 +0200

    adv api v0.00B

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,11 +4,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """Get number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'My-User-Agent'}
     r = requests.get(url, headers=headers, allow_redirects=False)
 
     if r.status_code >= 300:
         return 0
-
     return r.json().get('data').get('subscribers')
