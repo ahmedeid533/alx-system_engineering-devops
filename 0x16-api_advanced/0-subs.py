7209149d4d1be146a2f67da60f02446ffa69573b
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-16 14:21:47 +0200

    adv api v0.008

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'My-User-Agent'}
     r = requests.get(url, headers=headers, allow_redirects=False)
+
     if r.status_code >= 300:
         return 0
 
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,6 +7,7 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'My-User-Agent'}
     r = requests.get(url, headers=headers, allow_redirects=False)
+
     if r.status_code >= 300:
         print('None')
     else:
